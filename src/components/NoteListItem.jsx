4cde667d5b241a009cade517483ef3ada04a78fa
M Iqbal Praditya Oscar <89016678+miqbalpo@users.noreply.github.com>
2025-09-16 18:02:14 +0700

    added styling

--- src/components/NoteListItem.jsx
+++ src/components/NoteListItem.jsx
@@ -4,7 +4,7 @@ import { showFormattedDate } from "../utils/index.js";
 function NoteListItem({ id, title, body, createdAt, archived, onDelete, onArchive }) {
   return (
     <div className="w-64 p-3 space-x-10 rounded-md text-justify outline-2 outline-offset-2 outline-gray-500">
-      <h2 className="min-h-12 max-h-48 align-top font-semibold text-wrap truncate">{title}</h2>
+      <h2 className="min-h-12 max-h-48 align-top text-lg font-semibold text-wrap truncate">{title}</h2>
       <p>{showFormattedDate(createdAt)}</p>
       <p className="mt-5 text-sm w-full h-48 overflow-auto">{body}</p>
       <p className="my-5 text-sm">Status: 
